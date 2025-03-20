f2cbc0be9658a802182bf0b0523df503983f545a
Yugo1130 <mi.141130.mi63@gmail.com>
2025-03-20 08:02:09 +0000

    07-2-2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,6 +10,13 @@ class PostController extends Controller
 {
     public function index(post $post) //インポートしたPostをインスタンス化して$postとして使用。
     {
-        return $post->get();
+        // $post->get()はpostsテーブル内のすべてのデータを取得し、結果をCollection（Laravelのデータ管理クラス）として返す。
+        // SELECT * FROM posts;と同じ。
+        // return $post->get();
+        
+        // blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+        // with以降をつけることで、posts.indexのデータを表示するときに、Viewにデータ（posts）を一緒に渡すことができる。
+        // 変数名 => 値
+        return view('posts.index')->with(['posts' => $post->get()]);
     }
 }
